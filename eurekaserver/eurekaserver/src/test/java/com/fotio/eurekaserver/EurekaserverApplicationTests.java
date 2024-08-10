5b2ee2266ed6aebffd42786f165a0173b5e490b5
adias <jnrfotio@gmail.com>
2024-08-10 12:11:48 +0200

    add ereka server

---
+++ eurekaserver/eurekaserver/src/test/java/com/fotio/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fotio.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
